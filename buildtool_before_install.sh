6fcca8020b041aa89e75d12cc8e6fccc941a18a0
kench1994 <kench.h.wong@gmail.com>
2017-04-11 22:30:33 +0800

    personal use

---
+++ buildtool_before_install.sh
@@ -0,0 +1,2 @@
+#! /bin/sh
+sudo apt-get install gcc g++ binutils patch bzip2 flex bison make autoconf gettext texinfo unzip sharutils subversion libncurses5-dev ncurses-term zlib1g-dev asciidoc bash bc binutils bzip2 fastjar flex git-core g++ build-essential util-linux gawk libgtk2.0-dev intltool jikespg zlib1g-dev genisoimage libncurses5-dev libssl-dev patch perl-modules python2.7-dev rsync ruby sdcc unzip wget gettext xsltproc libboost1.55-dev libboost1.55-tools-dev libxml-parser-perl libusb-dev bin86 bcc bzr ecj sharutils openjdk-7-jdk zip gcc-multilib quilt
