6f07ceb9356b1c93c5b4925b92286e4a12538be5
kench1994 <kench.h.wong@gmail.com>
2017-08-23 10:07:12 +0800

    fiddler

--- Fiddler/clean.php
+++
@@ -1,8 +0,0 @@
-<?php
-$file = "output/Fiddler/manifest.json";
-$content = file_get_contents($file);
-$data = json_decode($content, true);
-unset($data["plugins"]);
-unset($data["update_url"]);
-$content = json_encode($data);
-file_put_contents($file, $content);
--- Fiddler/src/js/page/background.js
+++
@@ -1,4 +0,0 @@
-chrome.browserAction.onClicked.addListener(function(){
-    var url = chrome.extension.getURL("html/options.html");
-    window.open(url, "fiddler_option_page");
-});
